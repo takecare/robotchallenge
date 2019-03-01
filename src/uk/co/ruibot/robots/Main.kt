426b4666f607471222583a23e759ef3eea4f1a84
Rui Teixeira <rui@vazteixeira.org>
2019-03-01 09:09:56 +0000

    add direction and robot (pos)

--- src/uk/co/ruibot/robots/Main.kt
+++ src/uk/co/ruibot/robots/Main.kt
@@ -2,8 +2,26 @@ package uk.co.ruibot.robots
 
 fun main() {
     // TODO 1. read world size
+
+    do {
+        val line = readLine()
+        println("> $line")
+    } while (line != null)
+
     // TODO 2. read (on loop) robots [define breaking condition]
     // TODO 3. evaluate all robots
 }
 
 // TODO define domain & changes to it
+
+enum class Direction {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST
+}
+
+class Robot(
+    val x: Int,
+    val y: Int
+)

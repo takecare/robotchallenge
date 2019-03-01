288c1e37f7a8c52e402e37cd9a7f90f6a21e2894
Rui Teixeira <rui@vazteixeira.org>
2019-03-01 08:38:27 +0000

    add main with TODOs

---
+++ src/uk/co/ruibot/robots/Main.kt
@@ -0,0 +1,9 @@
+package uk.co.ruibot.robots
+
+fun main() {
+    // TODO 1. read world size
+    // TODO 2. read (on loop) robots [define breaking condition]
+    // TODO 3. evaluate all robots
+}
+
+// TODO define domain & changes to it
